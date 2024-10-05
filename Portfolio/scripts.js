7e4c0b025c9b5cdb0363941e44f59f5cf6a1b2e3
Rajarshi Saha <124190708+Rsaha-16@users.noreply.github.com>
2024-10-05 13:02:04 +0530

    Delete Portfolio directory

--- Portfolio/scripts.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".text", {
-    strings: ['Computer Science Student.', 'ML Learner.', 'Passionate Gamer.', 'Sporty Person.'],
-    typeSpeed: 50,
-    backSpeed:30,
-    backDelay:1000,
-    loop:true
-});
